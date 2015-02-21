2f8b1a082c325dd012bfe7e05c602e5b3487e181
nnat <narayan.natarajan@gmail.com>
2015-02-21 14:49:58 -0500

    Updated cachematrix.R with a inverse caching matrix which clears the inverse when matrix is updated. Call cacheSolve to update the inverse. if inverse is not found, cacheSolve returns NULL.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# A custom matrix object with a solved (inverted) matrix
+# held such that it does not need recomputation
+makeCacheMatrix <- function(mat = matrix()) {
+  invmat<-NULL
+  set <-function(newmat){
+    mat<<-newmat
+    invmat<<-NULL
+  }
+  get<-function() mat
+  setinv <- function(inv) invmat<<-inv
+  getinv <- function() invmat
+  list(set=set,get=get,
+       setinv=setinv,
+       getinv=getinv)  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- try(solve(data, ...))
+  if (class(inv)=="try-error"){
+    message("**** Solve failed.")
+    return (NULL)
+    
+  }
+  x$setinv(inv)
+  inv
 }
